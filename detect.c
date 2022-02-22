8a02855ba94a3638efd99e98ac7c0eac59f3c046
Mit Bailey <mitbailey99@gmail.com>
2022-02-22 16:28:45 -0500

    Fixes.

--- detect.c
+++ detect.c
@@ -1,7 +1,15 @@
 #include <signal.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <time.h>
 #include "gpiodev.h"
-#include "meb_print.h"
+
+static inline uint64_t get_time_now()
+{
+    static struct timespec ts;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    return ts.tv_sec * 1000000000ULL + ts.tv_nsec;
+}
 
 volatile sig_atomic_t done = 0;
 void sig_handler(int sig)
@@ -9,11 +17,17 @@ void sig_handler(int sig)
     done = 1;
 }
 
-static int counter = 0;
-void gpio_callback(void *vp)
+uint64_t counter = 0;
+uint64_t tnow = 0;
+
+// static int counter = 0;
+void gpio_callback(void *_fp)
 {
-    // Call back triggered.
-    dbprintlf("Callback triggered!");
+    FILE *fp = (FILE *)_fp;
+    tnow = get_time_now();
+    fwrite(&tnow, 0x1, sizeof(uint64_t), fp);
+    fwrite(&counter, 0x1, sizeof(uint64_t), fp);
+    counter++;
 }
 
 int main(int argc, char *argv[])
@@ -23,7 +37,7 @@ int main(int argc, char *argv[])
     {
         if (gpiodev_pinout == PINOUT_RPI)
         {
-            detect_pin = 13
+            detect_pin = 13;
         }
         else
         {
@@ -31,9 +45,15 @@ int main(int argc, char *argv[])
             return 0;
         }
     }
-    
+
+    FILE* fp = fopen("stepper.info.txt", "wb");
+    if (fp == NULL)
+    {
+        return -1;
+    }
+
     signal(SIGINT, &sig_handler);
-    gpioRegisterIRQ(detect_pin, GPIO_IRQ_RISE, &gpio_callback, &counter 1000);
+    gpioRegisterIRQ(detect_pin, GPIO_IRQ_RISE, &gpio_callback, fp, 1000);
     
     while(!done)
     {
@@ -41,5 +61,6 @@ int main(int argc, char *argv[])
     }
 
     gpioUnregisterIRQ(detect_pin);
+    fclose(fp);
     return 0;
 }
